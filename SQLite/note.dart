18a97b7315a167f5acfb6063fed874811fe52f35
Hira Amir <76943721+Hira-Amir@users.noreply.github.com>
2023-10-17 01:28:28 +0500

    Create note.dart

---
+++ SQLite/note.dart
@@ -0,0 +1,12 @@
+class Note {
+  int id;
+  String title;
+  String desscription;
+
+  var content;
+  Note({
+    required this.id,
+    required this.title,
+    required this.desscription,
+  });
+}
